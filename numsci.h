afbb2e63d064369733171749768bedf9154de0f3
Robert Brothers <rbrothers12@gmail.com>
2016-02-13 23:27:37 -0600

    added comments to numsci.h

--- numsci.h
+++ numsci.h
@@ -7,14 +7,19 @@
 
 #define H 0.001
 
+// compute the first order derivative via central difference method
 double diffOne(double (*function)(double x, double h), int x);
 
+// compute the second order derivative via central difference method
 double diffTwo(double (*function)(double x, double h), int x);
 
+// compute the third order derivative via central difference method
 double diffThree(double (*function)(double x, double h), int x);
 
+// compute the fourth order derivative via central difference method
 double diffFour(double (*function)(double x, double h), int x);
 
+// use the previous methods
 double diff(double (*function)(double x, double h), int x, int order);
 
 #endif
